6eb789b0bc4c616d7329cd593a107c460da4996f
lesha <oookosmos.m@yandex.ru>
2023-08-13 16:33:50 +0300

    s

--- src/constants/apiURI.js
+++ src/constants/apiURI.js
@@ -1,4 +1,3 @@
 
 export const MOVIES_API_URI =  "https://api.nomoreparties.co";
-export const API_URI = '//api.movie.diplom.student.nomoredomains.work/'
-//'http://localhost:3000'  
+export const API_URI = '//api.movie.diplom.student.nomoredomains.work/';  
